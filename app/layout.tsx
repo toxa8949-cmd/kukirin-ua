d1ebbd821733547c808f5916a2d550d5bde813ff
toxa8949-cmd <toxa8949@gmail.com>
2026-05-12 17:47:43 +0200

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import './kukirin-accents.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://kukirin.ua'),
